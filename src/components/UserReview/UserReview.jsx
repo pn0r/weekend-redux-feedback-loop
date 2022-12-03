4b61dd77a3458aeded84a327306325b1bfeea30f
Paul Norling <paulnorling87@gmail.com>
2022-12-03 13:26:16 -0600

    added axios

--- src/components/UserReview/UserReview.jsx
+++ src/components/UserReview/UserReview.jsx
@@ -1,15 +1,35 @@
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux'
+import axios from 'axios';
 
 function UserReview() {
   const feelings = useSelector(store => store.FeelingsReducer)
   const understanding = useSelector(store => store.UnderstandingReducer)
   const support = useSelector(store => store.SupportReducer)
   const comment = useSelector(store => store.CommentReducer)
-    const history = useHistory();
+  const history = useHistory();
+    
+  console.log("comment:", comment)  
+
     const navigateNext = () => {
-        history.push('/SubmitSucces');
+        
+        axios({
+          method: '/POST',
+          url: '/',
+          data: {
+            feeling: feelings,
+            understanding: understanding,
+            support: support,
+            comments: comment,
+          }
+        }).then((response) => {
+          console.log(response);
+          history.push('/SubmitSucces');
+      }).catch((err) => {
+          console.log("UserReview",err);
+      });
       };
+
     return (
       <div>
         <div>Review Your Feedback</div>
@@ -23,3 +43,4 @@ function UserReview() {
 }
 
 export default UserReview;
+
